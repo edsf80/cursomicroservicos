02cfb32d1a54f56f8f51aa90623432a066082388
Ednado Dilorenzo <ednaldo.dilorenzo@hotmail.com>
2018-10-19 14:56:57 -0300

    Adicionando api gateway e config server

---
+++ ecommerce-config/src/main/java/br/edu/ifpb/ecommerceconfig/EcommerceConfigApplication.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.ecommerceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EcommerceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceConfigApplication.class, args);
+	}
+}
